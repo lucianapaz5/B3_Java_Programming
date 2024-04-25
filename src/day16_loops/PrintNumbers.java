76797a1cc667a2e2b5a9f684850413d0cedc5907
lucianapazaguilera <lucianapaz5@gmail.com>
2024-04-24 22:21:16 -0400

    new strings and while loop

---
+++ src/day16_loops/PrintNumbers.java
@@ -0,0 +1,30 @@
+package day16_loops;
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+
+
+        // can you print all the even numbers up until 100 starting from 2;
+
+        int num = 2;
+
+        while (num <= 100){
+            System.out.println(num);
+            num += 2;
+        }
+
+
+        System.out.println("--------");
+        int i = 1;
+        while (i <= 100) {
+
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+
+            i++;
+        }
+
+
+    }
+}
